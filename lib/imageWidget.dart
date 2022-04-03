0e17b517328844f8067dddb2a40f76d667da308b
yeyoung8805 <yyk8805@naver.com>
2022-04-03 17:09:51 +0900

    app with image

---
+++ lib/imageWidget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ImageWidgetApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ImageWidgetApp();
+  }
+}
+
+class _ImageWidgetApp extends State<ImageWidgetApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Widget'),
+      ),
+      body: Container(
+          child: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('image/flutter_logo.png'),
+        ],
+      ))),
+    );
+  }
+}
